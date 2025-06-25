439702688a9d8584960328e2fb8884e939aef7ae
Nicholas Barros <81240462+NickRyanDev@users.noreply.github.com>
2025-06-25 17:13:35 -0300

    makes robot capable to make 90 degrees turns

---
+++ linefollower_90degrees.ino
@@ -0,0 +1,61 @@
+#define rightF 10
+#define rightB 11
+#define leftF 6
+#define leftB 5
+
+#define EN0 12
+#define EN1 2
+
+#define sensorLeft 8
+#define sensorRight 7
+
+bool sensorRightRead = false;
+bool sensorLeftRead = false;
+
+const int speed = 200;
+const int maxSpeed = 235;
+const int lowSpeed = 135;
+const int stopped = 0;
+
+
+void setup() {
+  pinMode(rightF, OUTPUT);
+  pinMode(rightB, OUTPUT);
+  pinMode(leftF, OUTPUT);
+  pinMode(leftB, OUTPUT);
+  pinMode(EN0, OUTPUT);
+  pinMode(EN1, OUTPUT);
+
+  pinMode(sensorLeft, INPUT);
+  pinMode(sensorRight, INPUT);
+
+  digitalWrite(EN0, HIGH);
+  digitalWrite(EN1, HIGH);
+}
+
+void loop() {
+  sensorLeftRead = digitalRead(sensorLeft);
+  sensorRightRead = digitalRead(sensorRight);
+  if((sensorLeftRead == HIGH) && (sensorRightRead == HIGH)){
+    analogWrite(leftB, stopped);
+    analogWrite(rightB, stopped);
+    analogWrite(leftF, maxSpeed);
+    analogWrite(rightF, maxSpeed);
+  }
+  if((sensorLeftRead == LOW) && (sensorRightRead == HIGH)){
+    analogWrite(leftF, stopped);
+    analogWrite(leftB, speed);
+    analogWrite(rightF, lowSpeed);
+  }
+  if((sensorLeftRead == HIGH) && (sensorRightRead == LOW)){
+    analogWrite(rightF, stopped);
+    analogWrite(leftF, lowSpeed);
+    analogWrite(rightB, speed);
+  }
+ if((sensorLeftRead == LOW) && (sensorRightRead == LOW)){
+    analogWrite(leftF, stopped);
+    analogWrite(rightF, stopped);
+    analogWrite(leftB, stopped);
+    analogWrite(rightB, stopped);
+  }
+}
